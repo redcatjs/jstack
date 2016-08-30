227fc32a8908af9e017cdc346d26e2fb07177858
jo <jo@surikat.pro>
2016-08-30 18:46:23 +0200

    add $.fn.removeClassPrefix

--- jstack.js
+++ jstack.js
@@ -4163,6 +4163,16 @@ String.prototype.ucfirst = function() {
 			}
         });
     };
+    
+    $.fn.removeClassPrefix = function(prefix) {
+		this.each(function(i, el) {
+			var classes = el.className.split(" ").filter(function(c) {
+				return c.lastIndexOf(prefix, 0) !== 0;
+			});
+			el.className = $.trim(classes.join(" "));
+		});
+		return this;
+	};
 
 } )( jQuery );
 
