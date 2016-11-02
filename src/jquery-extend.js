70edb06aa82bd87713cca04c08a1615ff0964c02
jo <jo@surikat.pro>
2016-11-02 13:55:15 +0100

    add reverse to jquery

--- src/jquery-extend.js
+++ src/jquery-extend.js
@@ -346,5 +346,9 @@
 	$.arrayCompare = function (a, b) {
 		return $(a).not(b).get().length === 0 && $(b).not(a).get().length === 0;
 	};
+	
+	$.fn.reverse = function(){
+		return $(this.get().reverse());
+	};
 
 } )( jQuery );
