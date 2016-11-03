011326088ca60d72e5908c9508a249f4f656d04a
jo <jo@surikat.pro>
2016-11-03 13:41:04 +0100

    $.fn.attrStartsWith

--- src/jquery-extend.js
+++ src/jquery-extend.js
@@ -244,5 +244,17 @@
 	$.fn.reverse = function(){
 		return $(this.get().reverse());
 	};
+	
+	 $.fn.attrStartsWith = function(s) {
+        var attrs = {};
+        this.each(function(index){
+            $.each(this.attributes, function(index, attr){
+                if(attr.name.indexOf(s)===0){
+                   attrs[attr.name] = attr.value;
+                }
+            });
+        });
+        return attrs;
+    };
 
 } )( jQuery );
