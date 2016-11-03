e841aba082779f31ef34a05d67420381db7e1561
jo <jo@surikat.pro>
2016-11-03 13:25:15 +0100

    attrStartsWith pseudo-selector

--- src/jquery-extend.js
+++ src/jquery-extend.js
@@ -83,6 +83,17 @@
 			return $( a ).parents( m[ 3 ] ).length < 1;
 		}
 	} );
+	
+	$.extend($.expr[':'], {
+		attrStartsWith: function (el, _, b) {
+			for (var i = 0, atts = el.attributes, n = atts.length; i < n; i++) {
+				if(atts[i].nodeName.toLowerCase().indexOf(b[3].toLowerCase()) === 0) {
+					return true; 
+				}
+			}
+			return false;
+		}
+	});
 
 	var findForks = {
 		"nth-level": function( selector, param ) {
