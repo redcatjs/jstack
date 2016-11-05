59de51c507a3a6044d3b091972ed566ed2051dd6
jo <jo@surikat.pro>
2016-11-06 00:08:00 +0100

    add j-component-jquery

--- src/component.js
+++ src/component.js
@@ -2,12 +2,20 @@ jstack.component = {};
 $.on('j:load','[j-component]',function(e){
 	var el = this;
 	var component = $(el).attr('j-component');
+	var componentJq = $(el).attr('j-component-jquery');
 	var config = $(el).dataAttrConfig('j-data-');
-	var load = function(){
-		var o = new jstack.component[component](el,config,e);
-		$(el).data('j:component',o);
-		
-	};
+	var load;
+	if(componentJq){
+		load = function(){
+			$(el)[componentJq](config);
+		};
+	}
+	else{
+		load = function(){
+			var o = new jstack.component[component](el,config,e);
+			$(el).data('j:component',o);			
+		};
+	}
 	if(jstack.component[component]){
 		load();
 	}
