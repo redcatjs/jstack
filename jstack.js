8d80514fe9e281fcf050297def759faa79e8a3cf
jo <jo@surikat.pro>
2016-11-06 00:46:46 +0100

    add extend helper

--- jstack.js
+++ jstack.js
@@ -1,4 +1,7 @@
 jstackClass = function(){};
+jstackClass.prototype.extend = function(c,parent){
+	c.prototype = Object.create(parent.prototype);
+};
 jstack = new jstackClass();
 ( function( $ ) {
 
--- src/core.js
+++ src/core.js
@@ -1,2 +1,5 @@
 jstackClass = function(){};
+jstackClass.prototype.extend = function(c,parent){
+	c.prototype = Object.create(parent.prototype);
+};
 jstack = new jstackClass();
