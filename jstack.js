e258c34040bc6beb6a5de48312652baa0d5938a4
jo <jo@surikat.pro>
2016-11-06 01:24:19 +0100

    improve reliability on jquery-component

--- jstack.js
+++ jstack.js
@@ -307,23 +307,15 @@ jstack = new jstackClass();
 	
 } )( jQuery );
 jstack.component = {};
-$.on('j:load','[j-component]',function(e){
+$()
+$.on('j:load','[j-component]',function(){
 	var el = this;
 	var component = $(el).attr('j-component');
-	var componentJq = $(el).attr('j-component-jquery');
 	var config = $(el).dataAttrConfig('j-data-');
-	var load;
-	if(componentJq){
-		load = function(){
-			$(el)[componentJq](config);
-		};
-	}
-	else{
-		load = function(){
-			var o = new jstack.component[component](el,config,e);
-			$(el).data('j:component',o);			
-		};
-	}
+	var load = function(){
+		var o = new jstack.component[component](el,config);
+		$(el).data('j:component',o);			
+	};
 	if(jstack.component[component]){
 		load();
 	}
@@ -331,7 +323,21 @@ $.on('j:load','[j-component]',function(e){
 		$js('jstack.'+component,load);
 	}
 });
-$.on('j:unload','[j-component]',function(e){
+$.on('j:load','[jquery-component]',function(){
+	var el = this;
+	var component = $(el).attr('jquery-component');
+	var config = $(el).dataAttrConfig('j-data-');
+	var load = function(){
+		$(el)[component](config);
+	};
+	if($.fn[component]){
+		load();
+	}
+	else{					
+		$js('jstack.jquery.'+component,load);
+	}
+});
+$.on('j:unload','[j-component]',function(){
 	var o = $(this).data('j:component');
 	if(o&&typeof(o.unload)=='function'){
 		o.unload();
--- src/component.js
+++ src/component.js
@@ -1,21 +1,13 @@
 jstack.component = {};
-$.on('j:load','[j-component]',function(e){
+$()
+$.on('j:load','[j-component]',function(){
 	var el = this;
 	var component = $(el).attr('j-component');
-	var componentJq = $(el).attr('j-component-jquery');
 	var config = $(el).dataAttrConfig('j-data-');
-	var load;
-	if(componentJq){
-		load = function(){
-			$(el)[componentJq](config);
-		};
-	}
-	else{
-		load = function(){
-			var o = new jstack.component[component](el,config,e);
-			$(el).data('j:component',o);			
-		};
-	}
+	var load = function(){
+		var o = new jstack.component[component](el,config);
+		$(el).data('j:component',o);			
+	};
 	if(jstack.component[component]){
 		load();
 	}
@@ -23,7 +15,21 @@ $.on('j:load','[j-component]',function(e){
 		$js('jstack.'+component,load);
 	}
 });
-$.on('j:unload','[j-component]',function(e){
+$.on('j:load','[jquery-component]',function(){
+	var el = this;
+	var component = $(el).attr('jquery-component');
+	var config = $(el).dataAttrConfig('j-data-');
+	var load = function(){
+		$(el)[component](config);
+	};
+	if($.fn[component]){
+		load();
+	}
+	else{					
+		$js('jstack.jquery.'+component,load);
+	}
+});
+$.on('j:unload','[j-component]',function(){
 	var o = $(this).data('j:component');
 	if(o&&typeof(o.unload)=='function'){
 		o.unload();
