bd2a0807d7721ee99595b7d8f21db22e7ce2beee
jo <jo@surikat.pro>
2016-11-18 23:05:00 +0100

    fix $.when behavior

--- src/controller.js
+++ src/controller.js
@@ -22,8 +22,13 @@ jstack.controller = function(controller){
 			dependencies.push(dependenciesData[i]);
 		}
 		var resolveDeferred = $.when.apply($, dependenciesData).then(function(){
-			for(var i = 0, l = arguments.length-2; i < l; i++){
-				args.push(arguments[i]);
+			if(dependenciesData.length==1){
+				args.push(arguments[0]);
+			}
+			else{
+				for(var i = 0, l = arguments.length; i < l; i++){
+					args.push(arguments[i][0]);
+				}
 			}
 		});
 		dependencies.push(resolveDeferred);
