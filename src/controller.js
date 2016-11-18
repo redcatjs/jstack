9fc6230999c6e4e9cce6c8db55ebae1b35020ff2
jo <jo@surikat.pro>
2016-11-18 15:36:32 +0100

    controller dependencies now support Deferred and so Ajax's Promise

--- src/controller.js
+++ src/controller.js
@@ -13,13 +13,32 @@ jstack.controller = function(id){
 			break;
 		}
 	}
+	var args = [];
 	if ( deps instanceof Array ) {
+		
+		var deferredObjects = [];
+		for(var i = 0, l = deps.length; i < l; i++){
+			var df = deps[i];
+			if(typeof(df)=='object'&&typeof(df.then)=='function'){
+				deferredObjects.push(df);
+			}
+		}
+		
+		if(deferredObjects.length){
+			var resolveDeferred = $.when.apply($, deferredObjects).then(function(){
+				for(var i = 0, l = arguments.length-2; i < l; i++){
+					args.push(arguments[i]);
+				}
+			});
+			deps.push(resolveDeferred);
+		}
+				
 		$js.require( deps, sync );
 	}
 
 	if ( fn ) {
 		var ctrl = function() {
-			return fn.apply( ctrl, arguments );
+			return fn.apply( ctrl, args );
 		};
 		ctrl.jstack = {};
 		jstack.controllers[ id ] = ctrl;
