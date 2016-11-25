1370ba17a87170ba83a90f29f01f890c0d1b4103
jo <jo@surikat.pro>
2016-11-25 02:38:37 +0100

    rebuild dist

--- jstack.js
+++ jstack.js
@@ -257,7 +257,6 @@ jstack = new jstackClass();
 					
 					if(full){
 						newObject = Object.deepObserve(newObject,callback,true,parts);
-						//rootObject[path.split('.').pop()] = newObject;
 						rootObject[skey] = newObject;
 					}
 					
@@ -802,6 +801,41 @@ jstack.reflection.arguments = function( f ) {
 		}
 	}
 	return r;
+};
+jstack.reflection.isCyclic = function(obj){
+  var keys = [];
+  var stack = [];
+  var stackSet = new Set();
+  var detected = false;
+
+  function detect(obj, key) {
+    if (typeof obj != 'object') { return; }
+    
+    if (stackSet.has(obj)) { // it's cyclic! Print the object and its locations.
+      var oldindex = stack.indexOf(obj);
+      var l1 = keys.join('.') + '.' + key;
+      var l2 = keys.slice(0, oldindex + 1).join('.');
+      console.log('CIRCULAR: ' + l1 + ' = ' + l2 + ' = ' + obj);
+      console.log(obj);
+      detected = true;
+      return;
+    }
+
+    keys.push(key);
+    stack.push(obj);
+    stackSet.add(obj);
+    for (var k in obj) { //dive on the object's children
+      if (obj.hasOwnProperty(k)) { detect(obj[k], k); }
+    }
+
+    keys.pop();
+    stack.pop();
+    stackSet.delete(obj);
+    return;
+  }
+
+  detect(obj, 'obj');
+  return detected;
 };
 $.arrayCompare = function (a, b) {
 	return $(a).not(b).get().length === 0 && $(b).not(a).get().length === 0;
