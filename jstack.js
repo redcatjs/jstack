59d82b958a5fc20db50a686e5e7320cc88be3180
jo <jo@surikat.pro>
2017-01-03 18:58:06 +0100

    rebuild dist

--- jstack.js
+++ jstack.js
@@ -2131,6 +2131,35 @@ $.fn.jComponentReady = function(callback){
 
   return FormSerializer;
 }));
+//from https://github.com/jayedul/html-minifier-prettifier
+$.prettifyHTML = function(el){
+	if(el.parent().length>0 && el.parent().data('assign')){
+		el.data('assign', el.parent().data('assign')+1);
+	}
+	else{
+		el.data('assign', 1);
+	}
+	if(el.children().length>0){
+		el.children().each(function(){
+			tbc='';
+			for(i=0; i<$(this).parent().data('assign'); i++)
+			{
+				tbc+='\t';
+			}
+			$(this).before('\n'+tbc);
+			$(this).prepend('\t');
+			$(this).append('\n'+tbc);
+			prettify($(this));				
+		});
+	}
+	else{
+		tbc='';
+		for(i=0; i<el.parent().data('assign'); i++){
+			tbc+='\t';
+		}
+		el.prepend('\n'+tbc);
+	}
+};
 jstack.template = {};
 jstack.template.templateVarSubstitutions = {};
 ( function( w, j ) {
@@ -2174,7 +2203,7 @@ jstack.template.templateVarSubstitutions = {};
 				if ( jstack.config.debug ) {
 					console.log( e );
 					console.log( compile );
-					console.log( html );
+					console.log( $.prettifyHTML(html) );
 				}
 			}
 		}
