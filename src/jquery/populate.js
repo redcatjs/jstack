a32aa1097d9f944ebe9c541aac98d15f2229a2e5
jo <jo@surikat.pro>
2017-01-12 11:12:52 +0100

    refactor wip

--- src/jquery/populate.js
+++ src/jquery/populate.js
@@ -1,5 +1,86 @@
 (function(){
 
+var populateSelect = function( input, value, config ) {
+	var isSelect2 = input.hasClass('select2-hidden-accessible');
+	if(input[0].hasAttribute('data-preselect')&&!isSelect2){
+		if(config.push){
+			var v = input.data('preselect') || [];
+			if(typeof(v)!='object'){
+				v = [v];
+			}
+			if(v.indexOf(value)===-1){
+				v.push(value);
+			}
+			input.data('preselect',v);
+		}
+		else{
+			input.data('preselect',value);
+		}
+		return;
+	}
+	
+	//if(input.hasClass('select2-hidden-accessible')){
+		//if(config.push){
+			//var v = input.val();
+			//if(v===null){
+				//v = [];
+			//}
+			//if(typeof(v)!='object'){
+				//v = [v];
+			//}
+			//if(v.indexOf(value)===-1){
+				//v.push(value);
+			//}
+			//console.log(input,value);
+			//setValue(input,value);
+		//}
+		//else{
+			//setValue(input,value);
+		//}
+		//if(!config.preventValEvent){
+			//input.trigger('change');
+		//}
+		//return;
+	//}
+	
+	var found = false;
+	$( "option", input ).each( function() {
+		if ( $( this ).val() == value ) {
+			$( this ).prop( "selected", true );
+			found = true;
+		}
+		else{
+			if(!config.push){
+				$( this ).prop( "selected", false );
+			}
+		}
+	} );
+	
+	if ( !found && config.addMissing && typeof(value)!='undefined' && value!==null ) {
+		var optionValue;
+		var optionText;
+		if($.type(value)=='object'){
+			optionValue = value.value;
+			optionText = value.text;
+		}
+		else{
+			optionValue = value;
+		}
+		if(typeof(optionText)=='undefined'){
+			optionText = optionValue;
+		}
+		if(!optionValue){
+			optionValue = optionText;
+		}
+		input.append( '<option value="' + optionValue + '" selected="selected">' + optionText + "</option>" );
+	}
+	
+	if(isSelect2&&!config.preventValEvent){
+		input.trigger('change');
+	}
+	
+};
+
 $.fn.populateInput = function( value, config ) {
 	config = $.extend({
 		addMissing: this.hasAttr('j-add-missing'),
@@ -17,102 +98,22 @@ $.fn.populateInput = function( value, config ) {
 			input.val(val);
 		};
 	}
-	var populateSelect = function( input, value ) {
-		var isSelect2 = input.hasClass('select2-hidden-accessible');
-		if(input[0].hasAttribute('data-preselect')&&!isSelect2){
-			if(config.push){
-				var v = input.data('preselect') || [];
-				if(typeof(v)!='object'){
-					v = [v];
-				}
-				if(v.indexOf(value)===-1){
-					v.push(value);
-				}
-				input.data('preselect',v);
-			}
-			else{
-				input.data('preselect',value);
-			}
-			return;
-		}
-		
-		//if(input.hasClass('select2-hidden-accessible')){
-			//if(config.push){
-				//var v = input.val();
-				//if(v===null){
-					//v = [];
-				//}
-				//if(typeof(v)!='object'){
-					//v = [v];
-				//}
-				//if(v.indexOf(value)===-1){
-					//v.push(value);
-				//}
-				//console.log(input,value);
-				//setValue(input,value);
-			//}
-			//else{
-				//setValue(input,value);
-			//}
-			//if(!config.preventValEvent){
-				//input.trigger('change');
-			//}
-			//return;
-		//}
-		
-		var found = false;
-		$( "option", input ).each( function() {
-			if ( $( this ).val() == value ) {
-				$( this ).prop( "selected", true );
-				found = true;
-			}
-			else{
-				if(!config.push){
-					$( this ).prop( "selected", false );
-				}
-			}
-		} );
-		
-		if ( !found && config.addMissing && typeof(value)!='undefined' && value!==null ) {
-			var optionValue;
-			var optionText;
-			if($.type(value)=='object'){
-				optionValue = value.value;
-				optionText = value.text;
-			}
-			else{
-				optionValue = value;
-			}
-			if(typeof(optionText)=='undefined'){
-				optionText = optionValue;
-			}
-			if(!optionValue){
-				optionValue = optionText;
-			}
-			input.append( '<option value="' + optionValue + '" selected="selected">' + optionText + "</option>" );
-		}
-		
-		if(isSelect2&&!config.preventValEvent){
-			input.trigger('change');
-		}
-		
-	};
 	return this.each(function(){
 		var input = $(this);
 		if(input.data('j:populate:prevent')) return;
 		if ( input.is( "select" ) ) {
 			if ( value instanceof Array ) {
 				if(input.attr('name').substr(-2)=='[]'||input.prop('multiple')){
-					populateSelect( input, value );
+					populateSelect( input, value, config );
 				}
 				else{
 					for ( var i = 0, l = value.length; i < l; i++ ) {
-						populateSelect( input, value[ i ] );
+						populateSelect( input, value[ i ], config );
 					}
 				}
 			}
 			else {
-				populateSelect( input, value );
+				populateSelect( input, value, config );
 			}
 		}
 		else if ( input.is( "textarea" ) ) {
