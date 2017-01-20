a39b1cdcc464bfdc56aadd65234238ca1d0cf328
jo <jo@surikat.pro>
2017-01-20 11:41:10 +0100

    clean

--- src/jquery/walkTheDom.js
+++ src/jquery/walkTheDom.js
@@ -2,23 +2,12 @@ $.walkTheDOM = function(node, func){
 	if(func(node)===false){
 		return false;
 	}
-	
 	var children = node.childNodes;
 	for(var i = 0, l = children.length; i < l; i++){
 		if(this.walkTheDOM(children[i], func)===false){
 			return false;
 		}
 	}
-	
-	/*
-	node = node.firstChild;
-	while(node){
-		if(this.walkTheDOM(node, func)===false){
-			return false;
-		}
-		node = node.nextSibling;
-	}
-	*/
 };
 $.fn.walkTheDOM = function(func){
 	var r = $();
