206758827426ffc53f19c7a41a87915b255a5a14
jo <jo@surikat.pro>
2017-01-31 17:29:21 +0100

    rebuild dist

--- jstack.js
+++ jstack.js
@@ -904,159 +904,193 @@ rawAsap.makeRequestCallFromTimer = makeRequestCallFromTimer;
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{}]},{},[2])(2)
 });
+(function(){
+var constructor = function(controllerSet,element){			
+	var self = this;
+	
+	var data = element.data('jModel') || {};
+	if(element[0].hasAttribute('j-view-inherit')){
+		var parent = element.parent().closest('[j-controller]');
+		if(parent.length&&element[0].hasAttribute('j-view-inherit')){
+			var inheritProp = element[0].getAttribute('j-view-inherit');
+			var parentData = parent.data('jModel') || {};
+			if(inheritProp){
+				data[inheritProp] = parentData;
+			}
+			else{
+				data = $.extend({},parentData,data);
+			}
+		}
+	}
+	
+	var defaults = {
+		domReady: function(){},
+		setData: function(){},
+	};
+	$.extend(true,this,defaults,controllerSet);
+	
+	this.element = element;
+	this.data = data;
+	element.data('jController',this);
+	
+	this.startDataObserver = function(){
+		self.data = ObjectObservable.create(self.data);
+		ObjectObservable.observe(self.data,function(change){
+			//console.log('j:model:update',change);
+			jstack.dataBinder.update();
+		});
+	};
+	
+	this.setDataArguments = [];
+	this.setDataCall = function(){
+		var r = this.setData.apply( this, this.setDataArguments );
+		if(r==false){
+			this.noRender = true;
+		}
+		else if($.type(r)=='object'&&r!==ctrl.data){
+			$.extend(this.data,r);
+		}
+		this.startDataObserver();
+	};			
+	
+	this.render = function(html){
+		if(this.noRender) return;
+		
+		html = $(html);
+		
+		//html.find(':input[name],[j-input],j-select[name]').each(function(){
+			//var key = jstack.dataBinder.getScopedInput(this);
+			//var val = jstack.dataBinder.getInputVal(this);
+			//jstack.dataBinder.dotSet(key,self.data,val,true);
+			
+		//});
+		
+		
+		var el = this.element;
+		el.data('jModel',this.data);
+		el[0].setAttribute('j-controller',this.name);
+		if(Boolean(el[0].getAttribute('j-view-append'))){
+			el.append( html );
+		}
+		else{
+			el.html( html );
+		}
+		this.domReady();
+	};
+	
+};
+
 jstack.controller = function(controller,element){
 	
 	if(typeof(controller)=='object'){
-		jstack.controllers[controller.name] = function(element){
-			
-			var self = this;
-			
-			var data = element.data('jModel') || {};
-			if(element[0].hasAttribute('j-view-inherit')){
-				var parent = element.parent().closest('[j-controller]');
-				if(parent.length&&element[0].hasAttribute('j-view-inherit')){
-					var inheritProp = element[0].getAttribute('j-view-inherit');
-					var parentData = parent.data('jModel') || {};
-					if(inheritProp){
-						data[inheritProp] = parentData;
-					}
-					else{
-						data = $.extend({},parentData,data);
-					}
-				}
+		
+		if(controller.extend){
+			if(!controller.dependencies){
+				controller.dependencies = [];
 			}
-			
-			var defaults = {
-				domReady: function(){},
-				setData: function(){},
-				data: data,
-			};
-			
-			
-			$.extend(true,this,defaults,controller);
-			
-			
-			this.ready = $.Deferred();
-			this.element = element;
-			element.data('jController',this);
-			
-			this.startDataObserver = function(){
-				self.data = ObjectObservable.create(self.data);
-				ObjectObservable.observe(self.data,function(change){
-					//console.log('j:model:update',change);
-					jstack.dataBinder.update();
+			var extend = $.Deferred();
+			controller.dependencies.push(extend);
+			var controllerPath = jstack.config.controllersPath+controller.extend;
+			$js.require(controllerPath);
+			$js(controllerPath,function(){
+				$.each(jstack.controllers[controller.extend],function(k,v){
+					if(typeof(controller[k])=='undefined'){						
+						controller[k] = v;
+					}
 				});
-			};
-			
-			this.setDataArguments = [];
-			this.setDataCall = function(){
-				var r = this.setData.apply( this, this.setDataArguments );
-				if(r==false){
-					this.noRender = true;
-				}
-				else if($.type(r)=='object'&&r!==ctrl.data){
-					$.extend(this.data,r);
-				}
-				this.startDataObserver();
-			};			
-			
-			this.render = function(html){
-				if(this.noRender) return;
-				
-				html = $(html);
-				
-				//html.find(':input[name],[j-input],j-select[name]').each(function(){
-					//var key = jstack.dataBinder.getScopedInput(this);
-					//var val = jstack.dataBinder.getInputVal(this);
-					//jstack.dataBinder.dotSet(key,self.data,val,true);
-					
-				//});
-				
-				
-				var el = this.element;
-				el.data('jModel',this.data);
-				el[0].setAttribute('j-controller',this.name);
-				if(Boolean(el[0].getAttribute('j-view-append'))){
-					el.append( html );
-				}
-				else{
-					el.html( html );
-				}
-				this.domReady();
-			};
-			
-		};
+				console.log('extended',controller.name);
+				extend.resolve();
+			});
+		}		
+		if(controller.mixins){
+			for(var i = 0, l = mixins.length;i<l;i++){
+				$.each(mixins[i],function(k,v){
+					if(typeof(controller[k])=='undefined'){
+						controller[k] = v;
+					}
+				});
+			}
+		}
+		
+		jstack.controllers[controller.name] = controller;
 		return jstack.controllers[controller.name];
 	}
-
 	
-	controller = jstack.controllers[controller] || jstack.controller($.extend(true,{name:controller},jstack.config.defaultController));
+	var controllerSet = jstack.controllers[controller] || jstack.controller($.extend(true,{name:controller},jstack.config.defaultController));
 	
-	controller = new controller(element);
-	
-	var name = controller.name;
+	var name = controllerSet.name;
+	var ready = $.Deferred();
 	
 	var dependencies = [];
 	
-	if(controller.dependencies&&controller.dependencies.length){		
+	if(controllerSet.dependencies&&controllerSet.dependencies.length){		
 		var dependenciesJsReady = $.Deferred();
-		$js(controller.dependencies,function(){
+		$js(controllerSet.dependencies,function(){
 			dependenciesJsReady.resolve();
 		});
 		dependencies.push(dependenciesJsReady);
 	}
 	
-	
-	var dependenciesData = controller.dependenciesData;
-	if(dependenciesData){
-		if(typeof(dependenciesData)=='function'){
-			controller.dependenciesData = dependenciesData = controller.dependenciesData();
-		}
-		if(dependenciesData&&dependenciesData.length){
-			var dependenciesDataRun = [];
-			for(var i = 0, l = dependenciesData.length; i < l; i++){
-				var dependencyData = dependenciesData[i];
-				if(typeof(dependencyData)=='function'){
-					dependencyData = dependencyData.call(controller);
-				}
-				
+	$.when.apply($, dependencies).then(function(){
+		
+		console.log('construct',controllerSet.name);
+		var controller = new constructor(controllerSet,element);
+		
+		var dependenciesDataReady = [];
+		var dependenciesData = controller.dependenciesData;
+		if(dependenciesData){
+			if(typeof(dependenciesData)=='function'){
+				controller.dependenciesData = dependenciesData = controller.dependenciesData();
+			}
+			if(dependenciesData&&dependenciesData.length){
+				var dependenciesDataRun = [];
+				for(var i = 0, l = dependenciesData.length; i < l; i++){
+					var dependencyData = dependenciesData[i];
+					if(typeof(dependencyData)=='function'){
+						dependencyData = dependencyData.call(controller);
+					}
 					
-				if($.type(dependencyData)=='object'){
-					if('abort' in dependencyData){
+						
+					if($.type(dependencyData)=='object'){
+						if('abort' in dependencyData){
+							var ddata = dependencyData;
+							dependencyData = $.Deferred();
+							(function(dependencyData){
+								ddata.then(function(ajaxReturn){
+									dependencyData.resolve(ajaxReturn);
+								});
+							})(dependencyData);
+						}
+					}
+					if(!($.type(dependencyData)=='object'&&('then' in dependencyData))){
 						var ddata = dependencyData;
 						dependencyData = $.Deferred();
-						(function(dependencyData){
-							ddata.then(function(ajaxReturn){
-								dependencyData.resolve(ajaxReturn);
-							});
-						})(dependencyData);
+						dependencyData.resolve(ddata);
 					}
-				}
-				if(!($.type(dependencyData)=='object'&&('then' in dependencyData))){
-					var ddata = dependencyData;
-					dependencyData = $.Deferred();
-					dependencyData.resolve(ddata);
-				}
-					
+						
 
-				dependenciesDataRun.push(dependencyData);
-			}
-			var resolveDeferred = $.when.apply($, dependenciesDataRun).then(function(){
-				for(var i = 0, l = arguments.length; i < l; i++){
-					controller.setDataArguments.push(arguments[i]);
+					dependenciesDataRun.push(dependencyData);
 				}
-			});
-			dependencies.push(resolveDeferred);
+				var resolveDeferred = $.when.apply($, dependenciesDataRun).then(function(){
+					for(var i = 0, l = arguments.length; i < l; i++){
+						controller.setDataArguments.push(arguments[i]);
+					}
+				});
+				dependenciesDataReady.push(resolveDeferred);
+			}
 		}
-	}
-	
-	$.when.apply($, dependencies).then(function(){
-		controller.setDataCall();
-		controller.ready.resolve();
+		
+		$.when.apply($, dependenciesDataReady).then(function(){
+			controller.setDataCall();
+			ready.resolve(controller);
+		});
+		
 	});
 	
-	return controller;
+	return ready;
 };
+
+})();
 jstack.url = (function(){
 	var Url = function(){};
 	var recursiveArrayToObject = function(o){
@@ -4278,9 +4312,9 @@ jstack.mvc = function(config){
 	
 	controllerReady.then(function(){
 		
-		var ctrl = jstack.controller(config.controller,target);
+		var ctrlReady = jstack.controller(config.controller,target);
 		
-		$.when(viewReady, ctrl.ready).then(function(view){
+		$.when(viewReady, ctrlReady).then(function(view,ctrl){
 			var html = view[0];
 			ctrl.render(html);
 			ready.resolve(target,ctrl);
