86d2f7bb50814e634ed599a0e55d43b549aea4d2
jo <jo@surikat.pro>
2017-02-04 00:53:37 +0100

    optimize

--- src/std/randomColor.js
+++ src/std/randomColor.js
@@ -1,8 +1,9 @@
 jstack.randomColor = function(){
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++ ) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
+    //var letters = '0123456789ABCDEF';
+    //var color = '#';
+    //for (var i = 0; i < 6; i++ ) {
+        //color += letters[Math.floor(Math.random() * 16)];
+    //}
+    //return color;
+    return '#'+Math.random().toString(16).substr(2,6);
 };
