4b18a56b5850abc4363df97c29805755f8d92be3
jo <jo@surikat.pro>
2017-02-09 15:25:15 +0100

    rebuild dist

--- jstack.js
+++ jstack.js
@@ -2556,6 +2556,38 @@ $.fn.jData = function(key){
 	
 	}
 };
+$.fn.onJstackReady = function(types,selector,data){
+	if ( typeof types === "object" ) {
+		if ( typeof selector !== "string" ) {
+			data = data || selector;
+			selector = undefined;
+		}
+		for(var type in types){
+			this.onJstackReady(type, selector, data, types[type]);
+		}
+		return this;
+	}
+	var params = [types];
+	var fn;
+	if(typeof selector === "string"){
+		params.push(selector);
+		fn = data;
+	}
+	else{
+		fn = selector;
+	}
+	params.push(function(){
+		var args = arguments;
+		var el = this;
+		return jstack.ready(function(){
+			fn.apply(el,args);
+		});
+	});
+	return this.on.apply(this,param);
+};
+$.onJstackReady = function(event,selector,callback){
+	return $(document).onReady(event,selector,callback);
+};
 (function(){
 	var templates = {};
 	var requests = {};
