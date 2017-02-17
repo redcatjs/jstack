8cf7a9f76b553454831db5b252e68f072634e032
jo <jo@surikat.pro>
2017-02-17 01:08:28 +0100

    wip

--- src/jquery/xhrPool.js
+++ src/jquery/xhrPool.js
@@ -1,10 +1,10 @@
 $.xhrPool = [];
-$.xhrPool.abortAll = function() {
+$.xhrPool.abortAll = function(){
 	$(this).each(function(i, jqXHR){
 		jqXHR.abort();
 		$.xhrPool.splice(i, 1);
 	});
-}
+};
 $(document).ajaxStart(function(jqXHR){
 	$.xhrPool.push(jqXHR);
 });
