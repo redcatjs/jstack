bb72089817f270a291edfbb734c229bc19899323
jo <jo@surikat.pro>
2017-03-02 23:19:08 +0100

    rebuild dist

--- jstack.js
+++ jstack.js
@@ -2637,6 +2637,52 @@ $(document).ajaxComplete(function(e,jqXHR){
 $.ajaxPrefilter(function(options, originalOptions, jqXHR){
 	jqXHR.jstackNS = options.namespace || jstack.ajaxNamespace;
 });
+//inspired from https://gist.github.com/infostreams/6540654
+$.fn.onFirst = function(which, handler) {
+      // ensures a handler is run before any other registered handlers, 
+      // independent of the order in which they were bound
+      var $el = $(this);
+      $el.off(which, handler);
+      $el.on(which, handler);
+
+      var events = $._data($el[0]).events;
+      var registered = events[which];
+      registered.unshift(registered.pop());
+
+      events[which] = registered;
+};
+
+//inspired from from https://github.com/nickyleach/jQuery.bindLast
+$.fn.onLast = function(event, cbFunc){
+	return this.each(function(){
+		var highIndex = 1000000;
+		var eventData = event.split('.');
+		var eventName = eventData[0];
+		
+		$(this).on(event, cbFunc);
+		
+		var events = $._data(this,'events'),
+			ourIndex = false,
+			usedIndicies = {};
+		
+		$.each(events[eventName], function(index, func){
+			if(func === cbFunc){
+				ourIndex = index;
+			}
+			
+			usedIndicies[index] = 1;
+		});
+		
+		if(ourIndex !== false){
+			while(usedIndicies[highIndex] == 1){
+				highIndex++;
+			}
+			
+			events[eventName][highIndex] = events[eventName][ourIndex];
+			delete events[eventName][ourIndex];
+		}
+	});
+};
 (function(){
 	var templates = {};
 	var requests = {};
@@ -2686,11 +2732,11 @@ jstack.loader = function(selector,handler,unloader){
 jstack.loader(':attrStartsWith("j-on-")',function(){
 	var $this = $(this);
 	var attrs = $this.attrStartsWith('j-on-');
+	var controller = jstack.dataBinder.getControllerObject(this);
 	$.each(attrs,function(k,v){
 		var event = k.substr(5);
 		$this[0].removeAttribute(k);
 		$this.on(event,function(e){
-			var controller = jstack.dataBinder.getControllerObject(this);
 			if(typeof(controller.methods)!='object'||typeof(controller.methods[v])!='function'){
 				throw new Error('Call to undefined method "'+v+'" by '+k+' and expected in controller '+controller.name);
 			}
