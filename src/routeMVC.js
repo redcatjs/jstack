46bf7874aad1a73db26c647ebdfd583cf9a1871a
jo <jo@surikat.pro>
2017-03-15 12:55:54 +0100

    up

--- src/routeMVC.js
+++ src/routeMVC.js
@@ -1,6 +1,8 @@
 jstack.routeMVC = function(path,obj){
 	return jstack.route(path,function(path,params,hash){
 		let container = $('[j-app]');
+		if(typeof(obj)=='string')
+			obj = {view:obj};
 		container.empty();
 		return jstack.mvc({
 			view:obj.view,
