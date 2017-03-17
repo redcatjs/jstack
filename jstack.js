ebbcaf9ff347949dc7b38abce703ee9aea4d0836
jo <jo@surikat.pro>
2017-03-17 07:04:28 +0100

    rebuild dist

--- jstack.js
+++ jstack.js
@@ -24,29 +24,29 @@ jstack.observe = function(obj, callback, rootObject){
 		}
 	});
 	let proxy = new Proxy(obj,{
-		get: function (target, key) {
-			//console.log('get',key);
+		get: function (target, key, receiver) {
 			if(key===prefix){
 				return true;
 			}
-			return target[key];
+			return Reflect.get(target,key,receiver);
 		},
-		set: function(target, key, value){
-			//console.log('set',key);
+		set: function(target, key, value, receiver){
 			if(typeof(value)=='object'&&value!==null){
 				value = jstack.observe(value,callback,obj);
 			}
-			target[key] = value;
+			let r = Reflect.set(target, key, value, receiver);
 			callback('set', {key:key, value:value}, target, rootObject);
-			return true;
+			return r;
 		},
 		deleteProperty: function (target, key) {
-			if(key in target){
-				delete target[key];
-				callback('unset', key, target, rootObject);
-			}
-			return true;
+			let r = Reflect.deleteProperty(target,key);
+			callback('unset', key, target, rootObject);
+			return r;
 		},
+		ownKeys: function(target){
+			return Reflect.ownKeys(target);
+		},
+		
 	});
 	return proxy;
 };
@@ -3029,7 +3029,11 @@ jstack.dataBinder = (function(){
 							var data = getData();
 							if(currentData===data) return;
 							currentData = data;
-
+							
+							if(!data){
+								data = [];
+							}
+							
 							var forIdList = [];
 							var collection = $( jfor.commentChildren().map(function(){
 								if(this.nodeType===Node.COMMENT_NODE&&this.nodeValue.split(' ')[0] == 'j:for:id'){
