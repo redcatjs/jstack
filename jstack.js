b7600a9cb22ac637642ac44c511b61cbeab6cb23
jo <jo@surikat.pro>
2017-03-27 19:38:15 +0200

    wip

--- jstack.js
+++ jstack.js
@@ -2463,23 +2463,29 @@ jstack.routeMVC = function(path,obj){
 jstack.ready = function(callback){
 	var when = $.Deferred();
 	
-	var defers = [ jstack.dataBinder.updateDeferStateObserver ];
-	if(jstack.dataBinder.loadingMutation>0){
-		var deferMutation = $.Deferred();
-		jstack.dataBinder.deferMutation.push(function(){
-			deferMutation.resolve();
+	setTimeout(function(){
+		
+		var defers = [ jstack.dataBinder.updateDeferStateObserver ];
+	
+		if(jstack.dataBinder.loadingMutation>0){
+			var deferMutation = $.Deferred();
+			jstack.dataBinder.deferMutation.push(function(){
+				deferMutation.resolve();
+			});
+			defers.push(deferMutation);
+		}
+		$.when.apply($,defers).then(function(){
+			when.resolve();
 		});
-		defers.push(deferMutation);
-	}
-	$.when.apply($,defers).then(function(){
-		when.resolve();
-	});
 
-	if(callback){
-		when.then(function(){
-			callback();
-		});
-	}
+		if(callback){
+			when.then(function(){
+				callback();
+			});
+		}
+		
+	});
+	
 	return when.promise();
 };
 
