13550e65006b5b113231cd6acc0a717243020274
jo <jo@surikat.pro>
2017-03-30 16:49:12 +0200

    add serializeForm jquery helper

--- src/jquery/serializeForm.js
+++ src/jquery/serializeForm.js
@@ -1,7 +1,9 @@
 $.fn.serializeForm = function(){
 	let data = {};
 	this.find(':input[name]').each(function(){
-		
+		let key = jstack.dataBinder.getScopedInput(this);
+		let val = jstack.dataBinder.getInputVal(this);
+		jstack.dataBinder.dotSet(key,data,val);
 	});
 	return data;
 };
