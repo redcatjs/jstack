34b5e3a7220e80db08066f51d2495258fe5453c1
jo <jo@surikat.pro>
2017-03-30 16:01:12 +0200

    rebuild dist

--- jstack.js
+++ jstack.js
@@ -1020,30 +1020,33 @@ jstack.traverseDom = function(node, func, asc){
 };
 (function(){
 
-let walkTheDOM = function(node, func){
-	if(func(node)===false){
-		return;
-	}
-	let children = node.childNodes;
-	for(let i = 0, l = children.length; i < l; i++){
-		if(!children[i]) continue;
-		walkTheDOM(children[i], func);
-	}
-};
 
-let dynWalkTheDOM = function(node, func){
+
+let walkTheDOM = function(node, func){
 	if(func(node)===false){
 		return;
 	}
 	let children = node.childNodes;
 	for(let i = 0; i < children.length; i++){
 		if(!children[i]) continue;
-		dynWalkTheDOM(children[i], func);
+		walkTheDOM(children[i], func);
 	}
 };
 
+/*
+let staticWalkTheDOM = function(node, func){
+	if(func(node)===false){
+		return;
+	}
+	let children = node.childNodes;
+	for(let i = 0, l = children.length; i < l; i++){
+		if(!children[i]) continue;
+		staticWalkTheDOM(children[i], func);
+	}
+};
+*/
+
 jstack.walkTheDOM = walkTheDOM;
-jstack.dynWalkTheDOM = dynWalkTheDOM;
 
 })();
 
