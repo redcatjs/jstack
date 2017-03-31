62ce4812babb47295e41d0fb24240b109d0badb2
jo <jo@surikat.pro>
2017-03-31 10:17:03 +0200

    re-enable uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -89,19 +89,17 @@ grunt.initConfig({
 		}
 	},
 	
-	/*
 	uglify: {
 		build: {
 			src: 'jstack.js',
 			dest: 'jstack.min.js'
 		}
 	},
-	*/
 	
 	watch: {
 		scripts: {
 			files: ['src/*.js','src/*/*.js','Gruntfile.js'],
-			//tasks: ['concat','uglify'],
+			tasks: ['concat','uglify'],
 			tasks: ['concat'],
 			options: {
 				debounceDelay: 250,
@@ -118,6 +116,6 @@ grunt.loadNpmTasks('grunt-contrib-watch');
 
 // cli exec
 grunt.registerTask('default', ['concat']);
-//grunt.registerTask('default', ['concat', 'uglify']);
+grunt.registerTask('default', ['concat', 'uglify']);
 
 };
