2f5b654d5a140534a0738674cec3e47abed83951
jo <jo@surikat.pro>
2017-03-31 23:18:13 +0200

    rebuild dist

--- jstack.js
+++ jstack.js
@@ -2003,38 +2003,6 @@ $.fn.jData = function(key){
 	}
 };
 
-$.fn.onJstackReady = function(types,selector,data){
-	if ( typeof types === "object" ) {
-		if ( typeof selector !== "string" ) {
-			data = data || selector;
-			selector = undefined;
-		}
-		for(var type in types){
-			this.onJstackReady(type, selector, data, types[type]);
-		}
-		return this;
-	}
-	var params = [types];
-	var fn;
-	if(typeof selector === "string"){
-		params.push(selector);
-		fn = data;
-	}
-	else{
-		fn = selector;
-	}
-	params.push(function(){
-		var args = arguments;
-		var el = this;
-		return jstack.ready(function(){
-			fn.apply(el,args);
-		});
-	});
-	return this.on.apply(this,params);
-};
-$.onJstackReady = function(event,selector,callback){
-	return $(document).onReady(event,selector,callback);
-};
 $.xhrPool = [];
 $.xhrPool.abortAll = function(namespace){
 	$(this).each(function(i, jqXHR){
@@ -2684,18 +2652,24 @@ class dataBinder {
 			let oldValue = dataBinder.dotGet(key,data);
 
 			value = dataBinder.dotSet(key,data,value);
+			
+			input.trigger('j:input:model',[value]);
+			
+			self.ready(function(){
+			
+				input.trigger('j:input',[value]);
+				if(eventType=='j:update'){
+					input.trigger('j:input:update',[value]);
+				}
+				else{
+					input.trigger('j:input:user',[value]);
+				}
 
-			input.trigger('j:input',[value]);
-			if(eventType=='j:update'){
-				input.trigger('j:input:update',[value]);
-			}
-			else{
-				input.trigger('j:input:user',[value]);
-			}
-
-			if(oldValue!==value){
-				input.trigger('j:change',[value,oldValue]);
-			}
+				if(oldValue!==value){
+					input.trigger('j:change',[value,oldValue]);
+				}
+			
+			});
 
 		};
 
