5ed684a08a231aef64ae0ee5d9c768be0623580b
jo <jo@surikat.pro>
2017-04-08 16:04:36 +0200

    clean

--- src/jquery/jData.js
+++ src/jquery/jData.js
@@ -1,35 +1,20 @@
 $.fn.jData = function(key){
 	if(this.length>1){
-		var a = [];
+		let a = [];
 		this.each(function(){
 			a.push( $.fn.jData.call( $(this), key ) );
 		});
 		return a;
 	}
 	else{
-			
-		var a = {};
-		var el = this[0];
-		
-		let controller = $(jstack.dataBinder.getController(el)).data('jController');
-		let dataBinder;
-		if(controller){
-			dataBinder = controller.dataBinder;
-		}
-		
-		$.each(this.attrStartsWith('j-data-'),function(k,v){
-			a[k] = v;
-		});
-		var data = {};
-		$.each(a,function(k,v){
+		let el = this[0];
+		let data = {};
+		this.attrStartsWith('j-data-').each(function(v,k){
 			$.attrsToObject( k.substr(7), v, data );
 		});
-		
 		if(key){
 			data = jstack.dataBinder.dotGet(key,data);
 		}
-		
 		return data;
-	
 	}
 };
