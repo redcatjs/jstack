07a55d489721d81dc40f35d20d6ffc3745864523
jo <jo@surikat.pro>
2017-04-08 11:00:10 +0200

    add object proto each + typo fix

--- src/prototype/object.js
+++ src/prototype/object.js
@@ -3,7 +3,7 @@ if(!Object.prototype.observable){
 		value: function(options){
 			return jstack.observable(this,options);
 		},
-		enumerable: false
+		enumerable: false,
 		writable: true,
 	});
 }
@@ -12,7 +12,7 @@ if(!Object.prototype.observe){
 		value: function(key,callback,namespace,recursive){
 			return jstack.observe(this,key,callback,namespace,recursive);
 		},
-		enumerable: false
+		enumerable: false,
 		writable: true,
 	});
 }
@@ -20,6 +20,19 @@ if(!Object.prototype.unobserve){
 	Object.defineProperty(Object.prototype, 'unobserve', {
 		value: function(key,callback,namespace,recursive){
 			return jstack.unobserve(this,key,callback,namespace,recursive);
+		},
+		enumerable: false,
+		writable: true,
+	});
+}
+if(!Object.prototype.each){
+	Object.defineProperty(Object.prototype, 'each', {
+		value: function(callback){
+			let o = this;
+			Object.keys(this).map(function(k){
+				callback(o[k],k,o);
+			});
+			return this;
 		},
 		enumerable: false,
 		writable: true,
