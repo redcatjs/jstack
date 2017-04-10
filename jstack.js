fcdced5701a739e852990301b949439ca2f69f1b
jo <jo@surikat.pro>
2017-04-10 14:43:23 +0200

    rebuild

--- jstack.js
+++ jstack.js
@@ -3713,13 +3713,14 @@ jstack.dataBindingElementCompiler.switch = {
 	},
 	callback(el,dataBinder,scope){
 		let jswitch = $('<!--j:switch-->');
-		let $this;
+		let $this = $(el);
 		
 		let myvar = el.getAttribute('j-switch');
 		el.removeAttribute('j-switch');
 
 		if(el.tagName.toLowerCase()=='template'){
 			$this.before(jswitch);
+			$this.after('<!--/j:switch-->');
 			let div = document.createElement('div');
 			div.appendChild( document.importNode(el.content, true) );
 			$(el).detach();
@@ -3727,29 +3728,54 @@ jstack.dataBindingElementCompiler.switch = {
 			$this = $(el);
 		}
 		else{
-			$this = $(el);
 			$this.append(jswitch);
+			$this.append('<!--/j:switch-->');
 		}
+		
+		let registerCase = function(casesStack,n){
+			let dom;
+			if(n.tagName.toLowerCase()=='template'){
+				let div = document.createElement('div');
+				div.appendChild( document.importNode(n.content, true) );
+				$(n).detach();
+				let domContents = $(div).contents();
+				dom = domContents.get();
+				domContents.each(function(){
+					dataBinder.compileDom( this, scope );
+				});
+			}
+			else{
+				dom = n;
+			}
+			casesStack.push({
+				val:n.hasAttribute('j-case')?n.getAttribute('j-case'):null,
+				dom:dom
+			});
+		};
+		
+		let cases = [];
+		let casesDefault = [];
+		$this.find('[j-case]').each(function(){
+			registerCase(cases,this);
+		});
+		$this.find('[j-case-default]').each(function(){
+			registerCase(casesDefault,this);
+		});
 		
 		$this.contents().each(function(){
 			dataBinder.compileDom( this, scope );
 		});
-
 		
-		let cases = $this.find('[j-case],[j-case-default]');
-
 		let currentData;
 		let render = function(){
 			let data = jstack.dataBinder.getValueEval(el,myvar,scope);
 			if(currentData===data) return;
 			currentData = data;
-			jstack.log(myvar,scope);
 
 			let found = false;
-			cases.filter('[j-case]').each(function(){
-				let jcase = $(this);
-				let caseVal = this.getAttribute('j-case');
-				if(caseVal==data){
+			cases.forEach(function(o){
+				let jcase = $(o.dom);
+				if(o.val==data){
 					jcase.insertAfter(jswitch);
 					found = true;
 				}
@@ -3757,8 +3783,8 @@ jstack.dataBindingElementCompiler.switch = {
 					jcase.detach();
 				}
 			});
-			cases.filter('[j-case-default]').each(function(){
-				let jcase = $(this);
+			casesDefault.forEach(function(o){
+				let jcase = $(o.dom);
 				if(found){
 					jcase.detach();
 				}
@@ -3769,8 +3795,9 @@ jstack.dataBindingElementCompiler.switch = {
 
 		};
 		
-		dataBinder.addWatcher(el,render);
+		dataBinder.addWatcher(jswitch[0],render);
 		render();
+		
 		
 		return false;
 	},
