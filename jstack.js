3374cd8c379166ce337584d81e9da6a934ca086b
jo <jo@surikat.pro>
2017-04-13 13:23:53 +0200

    rebuild

--- jstack.js
+++ jstack.js
@@ -3564,10 +3564,29 @@ jstack.dataBindingElementCompiler.jInclude = {
 	callback(n,dataBinder,scope){
 		let include = n.getAttribute('j-include');
 		n.removeAttribute('j-include');
-		$(n).empty();
-		let c = dataBinder.templates[include].clone().contents();
-		c.appendTo(n);
-		dataBinder.compileDom(n,scope);
+		
+		let tokens = jstack.dataBinder.textTokenizer(include);
+		if(tokens!==false){
+			include = dataBinder.compilerAttrRender(n,tokens,scope);
+		}
+		
+		let compile = function(){
+			$(n).empty();
+			let c = dataBinder.templates[include].clone().contents();
+			c.appendTo(n);
+			dataBinder.compileDom(n,scope);			
+		};
+		
+		if(dataBinder.templates[include]){
+			compile();
+		}
+		else{
+			$.ajax(include).then(function(html){
+				dataBinder.templates[include] = $('<html><rootnode>'+html+'</rootnode></html>');
+				compile();
+			});
+		}
+		
 		return false;
 	},
 };
