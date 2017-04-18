d70e8193e137903a5e43846158a5e09694a6805e
jo <jo@surikat.pro>
2017-04-19 00:58:59 +0200

    end template implementation

--- src/data-binding/j-javascript.js
+++ src/data-binding/j-javascript.js
@@ -4,7 +4,19 @@ jstack.dataBindingElementCompiler.push({
 		return (tagName=='script'||tagName=='template')&&n.getAttribute('type')=='text/j-javascript';
 	},
 	callback(n,dataBinder,scope){
-		let script = n.innerHTML;
+		let script;
+		
+		if(n.tagName.toLowerCase()=='template'){
+			let childNodes = document.importNode(n.content, true).childNodes;
+			script = '';
+			for(let i = 0, l = childNodes.length; i<l; i++){
+				script += childNodes[i].nodeValue;
+			}
+		}
+		else{
+			script = n.innerHTML;
+		}
+		
 		$(n).remove();
 		let func = new Function(script);
 		func.call(scope);
--- src/data-binding/j-template.js
+++ src/data-binding/j-template.js
@@ -4,6 +4,17 @@ jstack.dataBindingElementCompiler.push({
 		return (tagName=='script'||tagName=='template')&&n.getAttribute('type')=='text/j-template'&&n.id;
 	},
 	callback(n,dataBinder,scope){
+		
+		if(n.tagName.toLowerCase()=='template'){
+			let elements = document.importNode(n.content, true);
+			let div = document.createElement('div');
+			for(let i = 0, l = elements.length; i<l; i++){
+				div.appendChild(elements[i]);
+			}
+			jstack.copyAttributes(n,div);
+			n = div;
+		}
+		
 		dataBinder.templates[n.id] = $('<html><rootnode>'+n.innerHTML+'</rootnode></html>');
 		$(n).remove();
 		return false;
