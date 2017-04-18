3e9bf43d066197125f83e6db4ea6f7cb17eedaf4
jo <jo@surikat.pro>
2017-04-18 23:12:04 +0200

    fix

--- src/dom/walkTheDom.js
+++ src/dom/walkTheDom.js
@@ -5,17 +5,10 @@ let walkTheDOM = function(node, func){
 	if(func(node)===false){
 		return;
 	}
-	//let children = $.extend({},node.childNodes);
-	//for(let i = 0; i < children.length; i++){
-		//if(!children[i]) continue;
-		//walkTheDOM(children[i], func);
-	//}
-	let children = [];
-	for(let i = 0; i < node.childNodes.length; i++){
-		children.push(node.childNodes[i]);
-	}
-	while(children.length){
-		walkTheDOM(children.shift(), func);
+	let children = node.childNodes;
+	for(let i = 0; i < children.length; i++){
+		if(!children[i]) continue;
+		walkTheDOM(children[i], func);
 	}
 };
 
