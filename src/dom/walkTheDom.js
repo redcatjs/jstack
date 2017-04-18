dd2bd101c782626e46ccc5021bb51209b1eef90b
jo <jo@surikat.pro>
2017-04-19 00:20:51 +0200

    re-think walk the dom

--- src/dom/walkTheDom.js
+++ src/dom/walkTheDom.js
@@ -3,27 +3,13 @@
 
 let walkTheDOM = function(node, func){
 	if(func(node)===false){
-		return;
+		return false;
 	}
-	let children = node.childNodes;
-	for(let i = 0; i < children.length; i++){
-		if(!children[i]) continue;
-		walkTheDOM(children[i], func);
-	}
-};
 
-/*
-let staticWalkTheDOM = function(node, func){
-	if(func(node)===false){
-		return;
-	}
-	let children = node.childNodes;
-	for(let i = 0, l = children.length; i < l; i++){
-		if(!children[i]) continue;
-		staticWalkTheDOM(children[i], func);
-	}
+	Object.values(node.childNodes).forEach(function(n){
+		walkTheDOM(n, func);
+	});
 };
-*/
 
 jstack.walkTheDOM = walkTheDOM;
 
