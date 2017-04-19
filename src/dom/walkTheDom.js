8a3344e44647ab1576174f43507e2b148e7dd2e6
jo <jo@surikat.pro>
2017-04-19 19:35:13 +0200

    fix chrome

--- src/dom/walkTheDom.js
+++ src/dom/walkTheDom.js
@@ -5,8 +5,13 @@ let walkTheDOM = function(node, func){
 	if(func(node)===false){
 		return false;
 	}
-
-	Object.values(node.childNodes).forEach(function(n){
+	
+	let children = [];
+	let childNodes = node.childNodes;
+	for(let i = 0, l = childNodes.length; i<l ;i++){
+		children.push(childNodes[i]);
+	}
+	children.forEach(function(n){
 		walkTheDOM(n, func);
 	});
 };
