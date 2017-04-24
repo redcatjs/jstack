d0be2c8d687cbb208791ac12d66d3583c677bae2
jo <jo@surikat.pro>
2017-04-24 18:33:47 +0200

    add nocache option to walkTheDom

--- src/dom/walkTheDom.js
+++ src/dom/walkTheDom.js
@@ -1,18 +1,25 @@
 (function(){
 
 
-let walkTheDOM = function(node, func){
+let walkTheDOM = function(node, func, nocache){
 	if(func(node)===false){
 		return false;
 	}
 	
-	let children = [];
+	let children;
 	let childNodes = node.childNodes;
-	for(let i = 0, l = childNodes.length; i<l ;i++){
-		children.push(childNodes[i]);
+	if(nocache){
+		children = childNodes;
 	}
+	else{
+		children = [];
+		for(let i = 0, l = childNodes.length; i<l ;i++){
+			children.push(childNodes[i]);
+		}
+	}
+	
 	children.forEach(function(n){
-		walkTheDOM(n, func);
+		walkTheDOM(n, func, nocache);
 	});
 };
 
